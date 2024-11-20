764f104f153ce187589874031c95799c28d16a61
Erol Bayrasli <126ebayrasli@frhsd.com>
2024-11-20 12:38:18 -0500

    Video 4

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const titles = document.getElementsByClassName('title');
+const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+console.log(wmf);
 
-console.log(Array.isArray(titles));
-console.log(Array.isArray(Array.from(titles)));
-Array.from(titles).forEach(function(title){
-  console.log(title);
+var books = document.querySelector('#book-list li .name');
+console.log(books);
+
+books = document.querySelectorAll('#book-list li .name');
+console.log(books);
+
+Array.from(books).forEach(function(book){
+  console.log(book);
 });
